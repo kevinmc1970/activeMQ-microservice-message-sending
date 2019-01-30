f8f99bb7a16c0895c82724ae9a95305cc7a7e040
kevinmc1970 <mcintech@gmail.com>
2019-01-30 15:37:40 +0000

    initial

---
+++ messagetesting/src/main/java/com/virtualpairprogrammers/messagetesting/MessagetestingApplication.java
@@ -0,0 +1,34 @@
+package com.virtualpairprogrammers.messagetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+@SpringBootApplication
+public class MessagetestingApplication {
+
+	public static void main(String[] args) {
+		// from youtube tutorial https://www.youtube.com/watch?v=eqFgNouG490
+		// and https://www.youtube.com/watch?v=QTVmsI-oYd0
+		// can get spring context from .run call and then get JmsTemplate using it
+		ApplicationContext ctx = SpringApplication.run(MessagetestingApplication.class, args);
+		
+		// simple testing of JMS here
+		JmsTemplate jmsTemplate = ctx.getBean(JmsTemplate.class);
+		
+		// spring will convert second param to JMS message i.e. TextMessage or convert a hasmap into a MapMessage
+		jmsTemplate.convertAndSend("positionQueue", "this is a test");
+		
+		// N.B. springboot will create an ActiveMQ embedded queue if haven't create an external broker (i.e. "positionQueue")
+		// need to edit application.properties
+		// spring.activemq.broker-url = localhost:61616
+		
+		// when running this springboot app the the 'this is a test' message will appear in the queue 
+		// http://localhost:8161/admin/browse.jsp?JMSDestination=positionQueue
+		// after running broker C:\Users\becky\Documents\apache-activemq-5.15.8\bin>activemq start
+		// this queue is persisted BTW as created in admin console
+	}
+
+}
+
